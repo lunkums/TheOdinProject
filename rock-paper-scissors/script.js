8537ff7c188b58a0b7536d844716af3fd749e48f
Luke Leedy <lukeleedy123@gmail.com>
2022-07-23 15:09:44 -0400

    Add rounds script

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -17,3 +17,39 @@ function getComputerChoice() {
 
   return choice;
 }
+
+function round(playerSelection, computerSelection) {
+  const playerSelectionNice = properyNounify(playerSelection.toLowerCase());
+  let result;
+  let playerWins;
+
+  if (playerSelectionNice === computerSelection) {
+    return "It's a tie!";
+  }
+
+  switch (playerSelectionNice) {
+    case "Rock":
+      playerWins = computerSelection === "Scissors";
+      break;
+    case "Paper":
+      playerWins = computerSelection === "Rock";
+      break;
+    case "Scissors":
+      playerWins = computerSelection === "Paper";
+      break;
+    default:
+      return "It's a tie!";
+  }
+
+  if (playerWins) {
+    result = `You win! ${playerSelectionNice} beats ${computerSelection}`;
+  } else {
+    result = `You lose! ${computerSelection} beats ${playerSelectionNice}`;
+  }
+
+  return result;
+}
+
+function properNounify(noun) {
+  return noun.charAt(0).toUpperCase() + noun.slice(1);
+}
