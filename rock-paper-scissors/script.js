d0912f4678cce11e298b8478eaebc34f35d378a9
Luke Leedy <lukeleedy123@gmail.com>
2022-07-24 14:30:53 -0400

    Add game loop

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,55 +1,80 @@
-function getComputerChoice() {
-  const rand = Math.floor(Math.random() * 3);
-  let choice;
+const ROCK = "Rock";
+const PAPER = "Paper";
+const SCISSORS = "Scissors";
+const EXIT = "Exit";
 
-  switch (rand) {
-    case 0:
-      choice = "Rock";
+window.onload = game;
+
+function game() {
+  let playerInput;
+
+  while (true) {
+    playerInput = formatInput(prompt("Rock, paper, or scissors? "));
+
+    if (isValidInput(playerInput)) {
+      alert(playRound(playerInput, getComputerChoice()));
+    } else if (playerInput === EXIT) {
       break;
-    case 1:
-      choice = "Paper";
-      break;
-    case 2:
-    default:
-      choice = "Scissors";
-      break;
+    }
   }
-
-  return choice;
 }
 
-function round(playerSelection, computerSelection) {
-  const playerSelectionNice = properyNounify(playerSelection.toLowerCase());
+function isValidInput(input) {
+  return input === ROCK || input === PAPER || input === SCISSORS;
+}
+
+function playRound(playerSelection, computerSelection) {
   let result;
   let playerWins;
 
-  if (playerSelectionNice === computerSelection) {
+  if (playerSelection === computerSelection) {
     return "It's a tie!";
   }
 
-  switch (playerSelectionNice) {
-    case "Rock":
-      playerWins = computerSelection === "Scissors";
+  switch (playerSelection) {
+    case ROCK:
+      playerWins = computerSelection === SCISSORS;
       break;
-    case "Paper":
-      playerWins = computerSelection === "Rock";
+    case PAPER:
+      playerWins = computerSelection === ROCK;
       break;
-    case "Scissors":
-      playerWins = computerSelection === "Paper";
+    case SCISSORS:
+      playerWins = computerSelection === PAPER;
       break;
     default:
       return "It's a tie!";
   }
 
   if (playerWins) {
-    result = `You win! ${playerSelectionNice} beats ${computerSelection}`;
+    result = `You win! ${playerSelection} beats ${computerSelection}`;
   } else {
-    result = `You lose! ${computerSelection} beats ${playerSelectionNice}`;
+    result = `You lose! ${computerSelection} beats ${playerSelection}`;
   }
 
   return result;
 }
 
-function properNounify(noun) {
-  return noun.charAt(0).toUpperCase() + noun.slice(1);
+function getComputerChoice() {
+  const rand = Math.floor(Math.random() * 3);
+  let choice;
+
+  switch (rand) {
+    case 0:
+      choice = ROCK;
+      break;
+    case 1:
+      choice = PAPER;
+      break;
+    case 2:
+    default:
+      choice = SCISSORS;
+      break;
+  }
+
+  return choice;
+}
+
+function formatInput(input) {
+  const lowerCaseInput = input.toLowerCase().trim();
+  return lowerCaseInput.charAt(0).toUpperCase() + lowerCaseInput.slice(1);
 }
