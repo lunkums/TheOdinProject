80a0b9ae7de139e62bfa2ba02eb100c0f7f25bfa
Luke Leedy <lukeleedy123@gmail.com>
2022-07-26 12:01:13 -0400

    Add js 10 solution

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+  if (index < 1) {
+    return "OOPS";
+  }
 
+  let fibonacciNumbers = [1, 1];
+
+  for (let i = 2; i < index; i++) {
+    fibonacciNumbers.push(fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2]);
+  }
+  return fibonacciNumbers[index - 1];
 };
 
 // Do not edit below this line
