689def73bdb9384b65b8536584d1c7a44cc5f81c
Luke Leedy <lukeleedy123@gmail.com>
2022-07-25 20:57:21 -0400

    Add js 03 solution

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let reverse = "";
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reverse += string.charAt(i);
+  }
+
+  return reverse;
 };
 
 // Do not edit below this line
