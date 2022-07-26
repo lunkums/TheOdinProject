caf091d09b0af0ca0627fa4a161225b5a39e7dd7
Luke Leedy <lukeleedy123@gmail.com>
2022-07-25 20:49:12 -0400

    Add js 02 solution

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let output = "";
+  for (let i = 0; i < num; i++) {
+    output += string;
+  }
+  return output;
 };
 
 // Do not edit below this line
