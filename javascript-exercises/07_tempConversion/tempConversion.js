5522d4f364bc6f0054af51c812e20d6f0998df0b
Luke Leedy <lukeleedy123@gmail.com>
2022-07-26 09:35:27 -0400

    Add js 07 solution

--- javascript-exercises/07_tempConversion/tempConversion.js
+++ javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (fahrenheit) {
+  const celsius = ((fahrenheit - 32) * 5) / 9;
+  return parseFloat(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  const fahrenheit = (celsius * 9) / 5 + 32;
+  return parseFloat(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
