dbe8b166c2e2964d6c8a06c56cbd914362cbd454
Luke Leedy <lukeleedy123@gmail.com>
2022-07-25 21:34:45 -0400

    Add messy 04 js solution

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+  let args = Array.from(arguments);
+  const original = args.shift();
+  let newArray = [];
 
+  for (let i = 0; i < original.length; i++) {
+    let element = original[i];
+    for (let j = 0; j < args.length; j++) {
+      if (element === args[j]) {
+        j = args.length;
+      } else if (j === args.length - 1) {
+        newArray.push(element);
+      }
+    }
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
