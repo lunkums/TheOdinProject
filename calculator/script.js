2352c04547502791df496e5208312f23d0cb47a0
Luke Leedy <lukeleedy123@gmail.com>
2022-08-05 15:16:50 -0400

    Add basic operations

---
+++ calculator/script.js
@@ -0,0 +1,15 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
