c07e2cf7bdd9e2355496faadb59ba40bd48cb12a
Luke Leedy <lukeleedy123@gmail.com>
2022-08-05 15:20:30 -0400

    Refactor operations

--- calculator/script.js
+++ calculator/script.js
@@ -1,15 +1,14 @@
-function add(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+const math = {
+  add(a, b) {
+    return a + b;
+  },
+  subtract(a, b) {
+    return a - b;
+  },
+  multiply(a, b) {
+    return a * b;
+  },
+  divide(a, b) {
+    return a / b;
+  },
+};
