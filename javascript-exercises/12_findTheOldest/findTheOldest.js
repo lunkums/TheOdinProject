b510d6e821aa844416917413736c8e59aece109d
Luke Leedy <lukeleedy123@gmail.com>
2022-08-05 15:01:51 -0400

    Add js exercise 12 solution

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.sort((a, b) => (getAge(a) > getAge(b) ? -1 : 1)).at(0);
 };
+
+function getAge(person) {
+  return "yearOfDeath" in person
+    ? person.yearOfDeath - person.yearOfBirth
+    : new Date().getFullYear() - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
