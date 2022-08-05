1b8e22ec8b84dc3c9889e953e4dd77f88ca8f74a
Luke Leedy <lukeleedy123@gmail.com>
2022-08-05 15:38:38 -0400

    Add clear functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,3 +1,18 @@
+const displayWindow = document.querySelector("#display");
+const clearButton = document.querySelector("#clear-button");
+
+let firstNumber;
+let secondNumber;
+
+window.addEventListener("load", clear);
+clearButton.addEventListener("click", clear);
+
+function clear() {
+  displayWindow.textContent = "";
+  firstNumber = "";
+  secondNumber = "";
+}
+
 const math = {
   add(a, b) {
     return a + b;
