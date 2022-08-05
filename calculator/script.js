dd208566810b9cabe58630484f7fa0061757e4c8
Luke Leedy <lukeleedy123@gmail.com>
2022-08-05 15:26:01 -0400

    Add buttons

--- calculator/script.js
+++ calculator/script.js
@@ -12,3 +12,10 @@ const math = {
     return a / b;
   },
 };
+
+function operate(operator, a, b) {
+  if (operator in math) {
+    return math[operator](a, b);
+  }
+  return "Invalid operation";
+}
